fa80b03787408f0c7820b2b107f01e7c2527b949
Fábio Gomes <fabioxgn@gmail.com>
2013-03-10 11:45:34 -0300

    Updated Drop script to Copy required Indy10 units and cleanup unused files

--- Indy10/Drop.bat
+++ Indy10/Drop.bat
@@ -1,1 +1,22 @@
-del /s /q *.cfg* *.tmpl *.dpk *.bdsproj *.proj *.rc *.dproj *.ico
+@echo off
+SET /P INDYROOT=Enter Indy SVN folder:
+SET INDYLIB=%INDYROOT%\trunk\Lib
+
+IF NOT EXIST %INDYLIB% GOTO NOTINDY
+
+DEL /S /Q System\.
+DEL /S /Q Core\.
+DEL /S /Q Protocols\.
+
+COPY %INDYLIB%\Core Core
+COPY %INDYLIB%\System System
+COPY %INDYLIB%\Protocols Protocols
+DEL /s /q *.cfg* *.tmpl *.dpk *.bdsproj *.proj *.rc *.dproj *.ico *.resources *.res
+
+GOTO SUCCESS
+
+:NOTINDY
+echo This is not a valid Indy10 path
+
+:SUCCESS
+echo Indy10 updated. Good luck.
